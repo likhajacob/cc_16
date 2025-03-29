ee1a63c6572baa1d9425271e785ea3144ac9800a
Likha Jacob <likha.jacob03@gmail.com>
2025-03-29 19:15:08 -0400

    Add fetchProductsAsync using async/await and try/catch

--- cc_16.js
+++ cc_16.js
@@ -17,3 +17,16 @@ function fetchProductsThen() {
         handleError(error);
       });
   }
+  // Task 3 - fetching products
+async function fetchProductsAsync() {
+    try {
+      const response = await fetch('https://www.course-api.com/javascript-store-products');
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+      const products = await response.json();
+      displayProducts(products);
+    } catch (error) {
+      handleError(error);
+    }
+  }
